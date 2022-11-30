415912a0433e1e475235c26abeee7a4d832a1873
ramkrushna <ramkrushnaprabhu@gmail.com>
2022-11-30 17:37:16 +0530

    make it better

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.js"));
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
